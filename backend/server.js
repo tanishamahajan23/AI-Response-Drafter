9b63a25754c437e9197c62521c9dd02d48da0d34
Tanisha <tanishamahajan2310@gmail.com>
2026-06-26 13:43:41 +0530

    added frontend js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const port= process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use(express.json());
+app.post("/generate", (req, res) => {
+    console.log(req.body);
+    res.send("Data recieved.");
+});
+
+app.get("/", (req,res)=>{
+    cosole.log("Server working.")
+})
