33be3e47436ca5a0ba28aa3d1b2fb2b0d0c04a96
Tanisha <tanishamahajan2310@gmail.com>
2026-06-28 11:52:50 +0530

    deploying frontend

--- frontend/script.js
+++ frontend/script.js
@@ -51,7 +51,7 @@ form.addEventListener("submit", async (event) => {
              message: messageInput.value
          };
 
-    const response = await fetch("http://127.0.0.1:5000/generate", {
+    const response = await fetch("https://ai-response-drafter-backend.onrender.com/generate", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
