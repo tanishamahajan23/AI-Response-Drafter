77c628bf282c2694b372d4f3fceb48a7f031eb13
Tanisha <tanishamahajan2310@gmail.com>
2026-06-28 11:37:28 +0530

    deploying backend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     res.send("Server working.");
 });
 
-const port= 5000;
+const port= process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
